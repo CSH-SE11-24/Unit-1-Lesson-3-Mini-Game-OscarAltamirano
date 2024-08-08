cb8b1397e0538dcc74efe512c976eaa77d664364
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Get started!
+let name = prompt("What's your Username?")
+
+console.log("Welcome to my Server, " + name)
+
+let role =  prompt("What role do you want?(He/Him, She/Her, They/Them)")
+console.log(`Okay, ${name}, your role is now ${role}.`)
+
+let Age = prompt("What is your age?")
+Age = parseInt(Age)
+console.log(typeof Age)
+
+
+
+
+
+
+
